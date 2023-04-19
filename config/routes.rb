68ad400df810ffc6798729305f422bfe04185413
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-19 15:41:13 +0000

    [Start]bookers2

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

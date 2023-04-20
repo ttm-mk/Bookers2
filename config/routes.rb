eade2fff9f1d1c8855fe162c4fda1b5254f5b53d
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-20 21:33:16 +0000

    [Update]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get '/' => 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   
   resources :books, only: [:new, :index, :show, :edit] 

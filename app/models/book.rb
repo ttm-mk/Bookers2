7db0865fa5e7598a59d7a881922d921447ebe827
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-20 19:27:45 +0000

    [Update]ルーティング設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  
+  has_one attached :image
+  belongs_to :users
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+  root to: 'homes#top'
+  get '/' => 'homes#top'
   devise_for :users
+  
+  resources :books, only: [:new, :index, :show, :edit] 
+  resources :user, only: [:index, :show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

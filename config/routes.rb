03c5746290fd2584abcf9e60b553064157f9e989
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-24 14:47:40 +0000

    [Update]topレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :books, only: [:new, :index, :show, :edit] 
-  resources :user, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

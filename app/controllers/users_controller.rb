e31d65c521397c251c5585256f5ddf538515d919
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-27 14:07:11 +0000

    [Update]introduction表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = Book.all
+    @book = Book.new
   end
-
-
-
 
   def edit
     @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
   
   private
   
-  def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :books, only: [:new, :index, :show, :edit, :create, :new, :destroy] 
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
